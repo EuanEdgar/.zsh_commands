9d396a28d4a834247a47c4161fb9b405f339177c
EuanEdgar <euan@spydr.com>
2025-09-03 15:39:19 +0100

    Simplify function running on directory change
    Use chpwd_functions

--- apps/cd.sh
+++ apps/cd.sh
@@ -1,11 +1,2 @@
-cd(){
-  builtin cd "$@"
-  exit_code=$?
-
-  if [ $exit_code = 0 ]; then
-    set_folder_colour
-    set_node_version
-  fi
-
-  return $exit_code
-}
+chpwd_functions+=set_folder_colour
+chpwd_functions+=set_node_version
